2470d15bd81c720a420aff07d70937c73ff714a1
Egor <itpd2010@yandex.ru>
2021-08-21 01:41:34 +0300

    feat: update

--- src/index.js
+++ src/index.js
@@ -41,7 +41,6 @@ const MORSE_TABLE = {
       
 function decode(cipher) {
     let cipherLength = cipher.length;
-    // console.log(cipherLength)
     let dot = '.';
     let dash = '-';
     let space = ' ';
@@ -50,36 +49,31 @@ function decode(cipher) {
     for (let i = 0; i < cipherLength; i += 10) {
         let result = '';
         let tenLetter = cipher.slice(i, i + 10);
-        console.log(tenLetter);
+
         if (tenLetter === '**********') {
             result += space;
         } else {
 
-            
             for (let j = 0; j < tenLetter.length; j += 2) {
                 let twoLetter = tenLetter[j] + tenLetter[j + 1];
                 if (twoLetter === '00') {
                     result = result;
-                    // console.log(tenLetter);
                 }
-                
                 else if (twoLetter === '10') {
                     result = `${result}${dot}`;
                 }
                 else if (twoLetter === '11')  {
                     result = `${result}${dash}`;
                 }
-                // console.log('two' + twoLetter,j);
             }
         }
         if (result === ' ') {
             finalResult += ' '
         } else {
-        finalResult += MORSE_TABLE[result];
+            finalResult += MORSE_TABLE[result];
         }
-        // console.log(result)
     }
-    return finalResult // write your solution here
+    return finalResult 
 }
 
 module.exports = {
