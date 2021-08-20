439d5cdd081aca6fd1926c290a8e8562f5465c2d
Egor <itpd2010@yandex.ru>
2021-08-21 01:36:48 +0300

    feat: morse decodered

--- src/index.js
+++ src/index.js
@@ -37,8 +37,49 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+
+      
+function decode(cipher) {
+    let cipherLength = cipher.length;
+    // console.log(cipherLength)
+    let dot = '.';
+    let dash = '-';
+    let space = ' ';
+    let finalResult = '';
+
+    for (let i = 0; i < cipherLength; i += 10) {
+        let result = '';
+        let tenLetter = cipher.slice(i, i + 10);
+        console.log(tenLetter);
+        if (tenLetter === '**********') {
+            result += space;
+        } else {
+
+            
+            for (let j = 0; j < tenLetter.length; j += 2) {
+                let twoLetter = tenLetter[j] + tenLetter[j + 1];
+                if (twoLetter === '00') {
+                    result = result;
+                    // console.log(tenLetter);
+                }
+                
+                else if (twoLetter === '10') {
+                    result = `${result}${dot}`;
+                }
+                else if (twoLetter === '11')  {
+                    result = `${result}${dash}`;
+                }
+                // console.log('two' + twoLetter,j);
+            }
+        }
+        if (result === ' ') {
+            finalResult += ' '
+        } else {
+        finalResult += MORSE_TABLE[result];
+        }
+        // console.log(result)
+    }
+    return finalResult // write your solution here
 }
 
 module.exports = {
